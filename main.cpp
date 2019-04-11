ed97035eb4ebdbf63c7c7e87be1fa7b6ed44cec9
haeundae96 <haeundae96@naver.com>
2019-04-11 12:14:37 +0900

    Add files via upload
    Still failing because of HoughCircles function. Learn more about it, and make it automatic modifying the arguments for each frames.

--- main.cpp
+++ main.cpp
@@ -5,30 +5,59 @@ using namespace std;
 
 
 
+
 int main(void) {
-    Mat image = imread("/Users/mr.y/Desktop/biltest.png", IMREAD_GRAYSCALE);
-    Mat RecTable(image.size(), CV_32F);
+    Mat table = imread("/Users/mr.y/Desktop/biltest.png", IMREAD_COLOR);
+    CV_Assert(table.data);
 
-    const int r = image.rows;
-    const int p = image.cols;
-    const int q = 15;
+    vector<Mat> bgr;
+    split(table, bgr);
 
-    for (int i = 0; i < image.rows; ++i){
-        for (int j = 0; j < image.cols; ++j) {
-            // for Point(i, j) to make Point(x,y)
-            int x = r * i / (p - 2 * q * (r - j)) - q * (r - j) / (p * r - 2 * q * (r - j));
-            int y = j;
-            if (x < 0 || x > image.rows) continue;
-            RecTable.at<float>(x, y) = image.at<float>(i, j);
+    // test some combinations
+//    imshow("table", table);
+    //imshow("g - b", bgr[1] - bgr[0]);   // Yellow ball
+    //imshow("r - g", bgr[2] - bgr[1]);   // Red ball
+//    imshow("b - r", bgr[0] - bgr[2]);   //  better all balls detected
 
-        }
+    //imshow("b - g", bgr[0] - bgr[1]);
+    //imshow("g - r", bgr[1] - bgr[2]);   // all balls detected
+    //imshow("r - b", bgr[2] - bgr[0]);
+
+    Mat balls = bgr[0] - bgr[2];
+    vector<Vec3f> circles;
+
+    //test
+    imshow("balls", balls);
+    waitKey(0);
+
+    GaussianBlur(balls, balls, Size(5, 5), 2, 2);
+    threshold(balls, balls, 60, 255, THRESH_BINARY);
+    // test
+    imshow("balls", balls);
+    waitKey(0);
+
+    HoughCircles(balls, circles, HOUGH_GRADIENT, 2, 20, 100, 50, 5, 50); // 200, 100 couldn't understand
+
+    for(size_t i = 0; i < circles.size(); ++i)
+    {
+        Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+        int radius = cvRound(circles[i][2]);
+
+        circle(balls, center, radius, Scalar(0, 0, 255), 25);
+
+        //test printing out the coordinates
+        cout << "x: " << circles[i][0] << ", y: " << circles[i][1] << endl;
+        cout << "radius: " << circles[i][2] << endl << endl;
+
     }
 
-    imshow("Original Image", image);
-    imshow("Convoluted", RecTable);
-
+    imshow("balls", balls);
 
     waitKey(0);
+
+
+
+
     return 0;
 
 }
